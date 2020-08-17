03af90a3e8ba71ed86fb8810a8f70ad603ef3675
musfiqurcse <md.musfiqurcse@outlook.com>
2020-08-17 20:03:42 +0200

    [initial] project initialized

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# tuc-social-web
+# Building a Social Web Site for TUC
+### Description:
+
+
+
+### PDF Resources
+Werkezung
+
+https://github.com/JazzCore/python-pdfkit
---
+++ src/bookers/apps/account/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request,
+                                username=data['username'],
+                                password=data[' password'])
+
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated Users')
+                return HttpResponse('Disabled Account')
+            return HttpResponse('Invalid Login')
+        else:
+            form = LoginForm()
+            return render(request, 'account/login.html', {'form':form})
+
+
+# Create your views here.
